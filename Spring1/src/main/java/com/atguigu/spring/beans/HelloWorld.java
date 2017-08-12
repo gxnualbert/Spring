5470755e55a23ac1ce7f3b787091a7d0a2ad361a
gxnualberthome <albert.chenwesoft@gmail.com>
2017-08-12 13:01:52 +0800

    完成

--- Spring1/src/main/java/com/atguigu/spring/beans/HelloWorld.java
+++ Spring1/src/main/java/com/atguigu/spring/beans/HelloWorld.java
@@ -1,19 +1,14 @@
 package com.atguigu.spring.beans;
-
 /**
  * HelloWorld 类中，有一个属性name，提供了一个setname 方法。
  * 然后再hello方法中打印课一条语句 hello 。。。。
  * */
-
 public class HelloWorld {
 	private String name;
 	public void setName(String name) {
 		this.name = name;
 	}
-	
 	public void hello() {
 		System.out.println("hello: "+name);
 	}
-	
-	
 }
