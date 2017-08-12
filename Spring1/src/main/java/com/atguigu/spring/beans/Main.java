ecb77bd2d356ed01566c417431d52e163eca40c4
gxnualberthome <albert.chenwesoft@gmail.com>
2017-08-12 11:48:33 +0800

    first add

---
+++ Spring1/src/main/java/com/atguigu/spring/beans/Main.java
@@ -0,0 +1,16 @@
+package com.atguigu.spring.beans;
+
+public class Main {
+
+	public static void main(String[] args) {
+//		类可以访问其所在包中的所有类 《java教程 69页》
+//		创建HelloWord的一个对象
+		HelloWorld helloWorld=new HelloWorld();
+//		为name属性赋值
+		helloWorld.setName("atguigu");
+//		调用hello方法
+		helloWorld.hello();
+		
+	}
+
+}
